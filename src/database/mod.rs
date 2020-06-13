d90125bfc0afc5ded12f9eca6c099d483c900218
Parham Alvani <parham.alvani@gmail.com>
2020-06-13 22:08:48 +0430

    feat: Add Connect Method

---
+++ src/database/mod.rs
@@ -0,0 +1,8 @@
+use mongodb::{Client, Database};
+
+use crate::setting;
+
+pub async fn connect(cfg: &setting::Database) -> Database {
+    Client::with_uri_str(cfg.address()).await
+        .expect("mongodb connection failed").database(cfg.name())
+}
