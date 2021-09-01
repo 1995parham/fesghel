ae1d03f3ee6b84f829db7c6ba1879f60e4a8b587
Parham Alvani <1995parham@tuta.io>
2021-09-01 10:16:44 +0430

    fix: correct formatting issues

--- src/database/mod.rs
+++ src/database/mod.rs
@@ -3,6 +3,8 @@ use mongodb::{Client, Database};
 use crate::setting;
 
 pub async fn connect(cfg: &setting::Database) -> Database {
-    Client::with_uri_str(cfg.address()).await
-        .expect("mongodb connection failed").database(cfg.name())
+    Client::with_uri_str(cfg.address())
+        .await
+        .expect("mongodb connection failed")
+        .database(cfg.name())
 }
