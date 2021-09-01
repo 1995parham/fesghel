d8666e4ae7704386fd837738d4d52c423f9ac131
Parham Alvani <parham.alvani@gmail.com>
2021-09-01 10:07:16 +0430

    feat: update packages

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,15 @@
+mod database;
 mod handler;
 mod model;
 mod request;
 mod setting;
 mod store;
-mod database;
 
 use actix_web::{web, App, HttpServer};
 
 use setting::Settings;
 
-#[actix_rt::main]
+#[actix_web::main]
 async fn main() -> std::io::Result<()> {
     simple_logger::init_with_level(log::Level::Info).expect("logger initiation failed");
 
@@ -17,21 +17,19 @@ async fn main() -> std::io::Result<()> {
 
     let db = database::connect(setting.database()).await;
 
-    HttpServer::new( move || {
+    HttpServer::new(move || {
         let url_handler = handler::URL::new(store::URL::new(db.clone()));
 
         App::new()
-            .service(
-               url_handler.register(web::scope("/api"))
-            )
-            .service(
-                handler::Healthz::register(web::scope("/"))
-            )
+            .service(url_handler.register(web::scope("/api")))
+            .service(handler::Healthz::register(web::scope("/")))
     })
     .workers(12)
-    .bind(
-        format!("{}:{}", setting.server().host(), setting.server().port())
-        )?
+    .bind(format!(
+        "{}:{}",
+        setting.server().host(),
+        setting.server().port()
+    ))?
     .run()
     .await
 }
